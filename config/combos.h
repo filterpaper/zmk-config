754c01d06f351fcbb89d50cf989a96c6482c4c06
filterpaper <filterpaper@localhost>
2021-11-28 19:36:05 +0800

    Correct layer toggle thumb key

--- config/combos.h
+++ config/combos.h
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: MIT
 
 /* Key positions
-    0  1  2  3  4   5  6  7  8  9
-   10 11 12 13 14  15 16 17 18 19
-   20 21 22 23 24  25 26 27 28 29
-            30 31  32 33          */
+    0  1  2  3  4       5  6  7  8  9
+   10 11 12 13 14      15 16 17 18 19
+   20 21 22 23 24      25 26 27 28 29
+              30 31  32 33            */
 
 compatible = "zmk,combos";
 #define COMBO(name, keypress, keypos) \
@@ -20,7 +20,7 @@ COMBO(VOLU,	&kp C_VOL_UP,  5 15)
 COMBO(VOLD,	&kp C_VOL_DN, 15 25)
 
 // layer toggle
-COMBO(numb,	&tog NUM, 32 25)
+COMBO(numb,	&tog NUM, 33 25)
 
 // vimac
 COMBO(vimacf, &kp LS(LC(LA(LG(F)))), 12 13)
