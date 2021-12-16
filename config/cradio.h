21dba5c35d2dda0ee58f1aebb1574d747bd7eeb8
filterpaper <filterpaper@localhost>
2021-12-16 13:28:02 +0800

    Remove grave ESC behavior

--- config/cradio.h
+++ config/cradio.h
@@ -35,13 +35,6 @@
 #define Z_SLEEP	 &kp LA(LG(C_PWR))
 #define Z_SHUT	 &kp LA(LG(LC(C_PWR)))
 
-// Bottom row clipboard
-#define TH(k1,k2,k3,k4) &th LG(V) k1  &th LG(C)  k2 &th LG(X) k3  &th LG(Z) k4
-
-// Home row mods
-#define HRML(k1,k2,k3,k4) &th LSHFT k1  &th LALT k2  &th LCTRL k3  &th LGUI k4
-#define HRMR(k1,k2,k3,k4) &th LGUI k1  &th LCTRL k2  &th LALT k3  &th LSHFT k4
-
 // BT shorcuts
 #define Z_BT_CLR &bt BT_CLR
 #define Z_BT_0   &bt BT_SEL 0
@@ -50,3 +43,9 @@
 #define Z_USB    &out OUT_USB
 
 #define ________ &trans
+
+// Clipboard shortcuts
+#define TH(k1,k2,k3,k4) &th LG(V) k1  &th LG(C)  k2 &th LG(X) k3  &th LG(Z) k4
+// Home row mods
+#define HRML(k1,k2,k3,k4) &th LSHFT k1  &th LALT k2  &th LCTRL k3  &th LGUI k4
+#define HRMR(k1,k2,k3,k4) &th LGUI k1  &th LCTRL k2  &th LALT k3  &th LSHFT k4
