24fc8b766afb9bda27bf69bc04a63c040785e3e8
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-01-07 19:56:23 +0800

    Streamline mod shortcuts

--- config/combos.h
+++ config/combos.h
@@ -23,6 +23,3 @@ COMBO(numb,	&tog NUM, 33 25)
 // vimac
 COMBO(vimacf, &kp LS(LC(LA(LG(F)))), 12 13)
 COMBO(vimacj, &kp LS(LC(LA(LG(J)))), 16 17)
-// discord
-COMBO(dis_up, &kp LS(LA(UARW)),  8 18)
-COMBO(dis_dn, &kp LS(LA(DARW)), 18 28)
