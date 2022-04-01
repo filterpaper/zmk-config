04a611f3b9c7f1b4e9dd55abfc7382230d941742
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-04-01 18:26:39 +0800

    Tweak tapping term and macro positions

--- config/cradio.h
+++ config/cradio.h
@@ -48,7 +48,7 @@
 // Hold tap key timers
 #define QUICK_TAP    <180>
 #define TAPPING_TERM <200>
-#define PHT_TERM     <230>
+#define PHT_TERM     <250>
 
 // Clipboard shortcut hold tap
 #define HT(k1,k2,k3,k4) &ht LG(V) k1  &ht LG(C)  k2 &ht LG(X) k3  &ht LG(Z) k4
@@ -75,7 +75,7 @@ name: name##_macro {                  \
 	label = #name;                    \
 	compatible = "zmk,behavior-macro";\
 	#binding-cells = <0>;             \
-	wait-ms = <1>;                    \
 	tap-ms = <1>;                     \
+	wait-ms = <1>;                    \
 	bindings = <keys>;                \
 };
