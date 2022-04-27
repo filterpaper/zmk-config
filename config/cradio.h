4eda9554a36e9d7d3df8fdf6071828c85c6b152e
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-04-27 18:36:09 +0800

    Add Ctrl Alt keys to positional hold tap, enable global quick tap

--- config/cradio.h
+++ config/cradio.h
@@ -46,9 +46,8 @@
 #define ________ &trans
 
 // Hold tap key timers
-#define QUICK_TAP    <180>
+#define QUICK_TAP    <130>
 #define TAPPING_TERM <200>
-#define PHT_TERM     TAPPING_TERM
 
 // Clipboard shortcut hold tap
 #define HT(k1,k2,k3,k4) &ht LG(V) k1  &ht LG(C)  k2 &ht LG(X) k3  &ht LG(Z) k4
