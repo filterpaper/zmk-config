037ddde08dd1e5508264a665c4d8c57b1777d1d3
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-05-31 20:24:31 +0800

    Increase tapping and quick tap term

--- config/cradio.h
+++ config/cradio.h
@@ -47,8 +47,8 @@
 #define ________ &trans
 
 // Hold tap key timers
-#define QUICK_TAP    <130>
-#define TAPPING_TERM <200>
+#define QUICK_TAP    <150>
+#define TAPPING_TERM <220>
 
 // Clipboard shortcut hold tap
 #define HT(k1,k2,k3,k4) &ht LG(V) k1  &ht LG(C)  k2 &ht LG(X) k3  &ht LG(Z) k4
