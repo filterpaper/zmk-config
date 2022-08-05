9893d3e2a8fd9601a428bd3578deb76af56abfc9
Albert Y <76888457+filterpaper@users.noreply.github.com>
2022-08-05 13:11:28 +0800

    Add selective positional hold tap

--- config/cradio.h
+++ config/cradio.h
@@ -39,17 +39,26 @@
 #define NAV_DN   &kp LS(LA(DARW))
 #define ________ &trans
 
-// Hold tap key timers
+// Hold tap macros
+#define TAPPING_TERM 210
 #define QUICK_TAP    125
-#define TAPPING_TERM 200
-#define SHIFT_TAPPING_TERM 180
-
 // Clipboard shortcuts
-#define HT(k1,k2,k3,k4) &ht LG(V) k1  &ht LG(C)  k2 &ht LG(X) k3  &ht LG(Z) k4
-
+#define HT(k1,k2,k3,k4) &ht LG(V) k1  &ht LG(C) k2  &ht LG(X) k3  &ht LG(Z) k4
 // Home row mods hold tap
-#define HRML(k1,k2,k3,k4) &sht LSHFT k1  &ht LALT k2  &ht LCTRL k3  &ht LGUI k4
-#define HRMR(k1,k2,k3,k4) &ht RGUI k1  &ht RCTRL k2  &ht RALT k3  &sht RSHFT k4
+#define HRML(k1,k2,k3,k4) &lht LSHFT k1  &lht LALT k2  &lht LCTRL k3  &lht LGUI k4
+#define HRMR(k1,k2,k3,k4) &rht RGUI k1  &rht RCTRL k2  &rht RALT k3  &rht RSHFT k4
+
+#define LEFT_HOLD_TRIGGER_KEYS    \
+					5  6  7  8  9 \
+	10 11 12 13    15 16 17 18 19 \
+				   25 26 27 28 29 \
+		  30 31 32 33
+
+#define RIGHT_HOLD_TRIGGER_KEYS   \
+	 0  1  2  3  4                \
+	10 11 12 13 14    16 17 18 19 \
+	20 21 22 23 24                \
+				30 31 32 33
 
 // Combo macro
 #define COMBO(name, keypress, keypos) \
